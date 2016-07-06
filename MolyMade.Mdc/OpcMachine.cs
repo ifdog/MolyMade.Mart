af5ed5d3961b0cbe843185fce79289fcbfbf4c5a
ifdog <zouyuanpeng@163.com>
2016-07-06 22:25:47 +0800

    Minor changes

---
+++ MolyMade.Mdc/OpcMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MolyMade.Mdc
+{
+    class OpcMachine:Machine
+    {
+        public OpcMachine(string name, int id, string path, int failures, MachineTypes type,
+            Dictionary<string, string> tags) : base(name, id, path, failures, type, tags)
+        {
+            
+        }
+        
+    }
+}
