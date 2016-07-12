26af3f49c2187eb1eb3daf51f4e286192265179e
ifdog <zouyuanpeng@163.com>
2016-07-12 22:37:29 +0800

    minor change

--- MolyMade.Mdc/Controller.cs
+++ MolyMade.Mdc/Controller.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MolyMade.Mdc
 {
-    class Controller
+    public class Controller
     {
+        private Hashtable OpcMachines;
+        public Controller()
+        {
+            
+        }
+
+        public void ReadConfig()
+        {
+            var x = System.Configuration.ConfigurationManager.AppSettings["NewKey0"];
+            int a = 1;
+        }
     }
 }
--- MolyMade.Tester/Form1.cs
+++ MolyMade.Tester/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MolyMade.Mdc;
 
 namespace MolyMade.Tester
 {
@@ -15,6 +16,12 @@ namespace MolyMade.Tester
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MolyMade.Mdc.Controller c = new Controller();
+            c.ReadConfig();
         }
     }
 }
