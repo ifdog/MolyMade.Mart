197e129b53fa37f7078a770ceb9a0412408d3198
ifdog <zouyuanpeng@163.com>
2016-07-26 20:51:17 +0800

    add config

---
+++ MolyMade.Mdc/Configurer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IniParser;
+using IniParser.Model;
+
+namespace MolyMade.Mdc
+{
+    public class Configurer:IDisposable
+    {
+        internal IniData IniData;
+        public string iniPath { get; internal set; }
+        public string[] ServerStrings { get; internal set; }
+        public Configurer(string iniPath)
+        {
+            this.iniPath= iniPath;
+        }
+
+        public void Load()
+        {
+            FileIniDataParser parser = new FileIniDataParser();
+            IniData = parser.ReadFile(this.iniPath);
+        }
+
+
+        public void Dispose()
+        {
+            throw  new NotImplementedException();
+        }
+    }
+}
